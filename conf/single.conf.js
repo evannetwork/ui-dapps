7b8c09dc4c68295e9e4d82193dcf76c86c0c76e1
Sebastian Dechant <sebastian.dechant@evan.team>
2018-12-28 21:53:26 +0100

    add custom hook for status update

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -16,6 +16,18 @@ nightwatch_config = {
         'browserstack.debug': true,
         'browser': 'chrome'
       }
+    },
+    beforeEach: function (browser, done) {
+      if (this.test_settings.selenium_host === 'hub.browserstack.com') {
+        return require('nightwatch-browserstack').storeSessionId(browser, done)
+      }
+      done()
+    },
+    afterEach: function (browser, done) {
+      if (this.test_settings.selenium_host === 'hub.browserstack.com') {
+        return require('nightwatch-browserstack').updateStatus(browser, done)
+      }
+      done()
     }
   }
 };
