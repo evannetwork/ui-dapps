0cf06745941105c5bcf163c4b942f1fb2e0648a5
Sebastian Dechant <sebastian.dechant@evan.team>
2018-12-28 22:28:15 +0100

    fix tests, add reporting to browserstack

--- conf/globals_module.js
+++ conf/globals_module.js
@@ -1,14 +1,42 @@
 module.exports = {
   beforeEach: function (browser, done) {
-    if (this.test_settings.selenium_host === 'hub.browserstack.com') {
-      return require('nightwatch-browserstack').storeSessionId(browser, done)
+    if (this.client.options.selenium_host === 'hub-cloud.browserstack.com') {
+      return storeSessionId(browser, done)
     }
     done()
   },
   afterEach: function (browser, done) {
-    if (this.test_settings.selenium_host === 'hub.browserstack.com') {
-      return require('nightwatch-browserstack').updateStatus(browser, done)
+    if (this.client.options.selenium_host === 'hub-cloud.browserstack.com') {
+      return updateStatus(browser, done)
     }
+    done()
+  }
+};
+
+
+function storeSessionId(browser, done) {
+  browser.session(function (session) {
+    browser.browserStackSessionId = session.sessionId
+    done()
+  })
+};
+function updateStatus(browser, done) {
+  if (browser.currentTest.results.failed || browser.currentTest.results.errors) {
+    const caps = browser.options.desiredCapabilities
+    const user = caps['browserstack.user']
+    const key = caps['browserstack.key']
+    const options = {
+      host: 'api.browserstack.com',
+      path: `/automate/sessions/${browser.browserStackSessionId}.json`,
+      method: 'PUT',
+      auth: `${user}:${key}`,
+      headers: {'Content-Type': 'application/json'}
+    }
+    require('https')
+      .request(options, function () { done() })
+      .on('error', function (error) { throw error })
+      .end(JSON.stringify({status: 'error', reason: browser.currentTest.results.lastError.message}))
+  } else {
     done()
   }
 };
