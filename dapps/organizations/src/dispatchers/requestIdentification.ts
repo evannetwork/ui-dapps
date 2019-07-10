a0d3aac5239711dcb320d8f8aea5b7169e831c66
S3bb1 <sebastian.dechant@evan.team>
2019-07-10 13:40:41 +0200

    add bmail dispatcher

--- dapps/organizations/src/dispatchers/requestIdentification.ts
+++ dapps/organizations/src/dispatchers/requestIdentification.ts
@@ -38,9 +38,75 @@ const dispatcher = new Dispatcher(
   '_org.dispatchers.request-identification'
 );
 
+/**
+ * creates a new keyexchange with a target account and stores it at the given alias
+ *
+ * @param      {any}     runtime    runtime object
+ * @param      {string}  targetAcc  target keyExchange accountid
+ * @param      {string}  alias      alias of the keyexchanged accountid
+ */
+async function doKeyExchange(runtime: any, targetAcc: string, alias: string) {
+  const myAccountId = runtime.activeAccount;
+
+  const profile = new bcc.Profile({
+    accountId: targetAcc,
+    contractLoader: runtime.contractLoader,
+    dataContract: runtime.dataContract,
+    defaultCryptoAlgo: 'aes',
+    executor: runtime.executor,
+    ipld: runtime.ipld,
+    log: runtime.logger.log,
+    nameResolver: runtime.nameResolver,
+    rightsAndRoles: runtime.rightsAndRoles,
+  });
+
+  const targetPubKey = await profile.getPublicKey();
+  const commKey = await runtime.keyExchange.generateCommKey();
+  await runtime.keyExchange.sendInvite(targetAcc, targetPubKey, commKey, {});
+
+  // add key to profile
+  await runtime.profile.addContactKey(
+    targetAcc,
+    'commKey',
+    commKey
+  );
+  await runtime.profile.addProfileKey(
+    targetAcc, 'alias', alias
+  );
+
+  await runtime.profile.storeForAccount('addressBook');
+}
+
+
 dispatcher
   .step(async (instance: DispatcherInstance, data: any) => {
     const runtime = instance.runtime;
+
+
+    // check if key exchange with the smart agents exist
+    if (!await runtime.profile.getContactKey('0x74479766e4997F397942cc607dc59f7cE5AC70b2', 'commKey')) {
+      await doKeyExchange(runtime, '0x74479766e4997F397942cc607dc59f7cE5AC70b2', 'Notary Smart Agent')
+    }
+
+    const bmail = {
+      content: {
+        from: runtime.activeAccount,
+        to: '0x74479766e4997F397942cc607dc59f7cE5AC70b2',
+        title: 'Notary verification request',
+        body: 'This is a little example to demonstrate sending a bmail.',
+        attachments: [{
+          type: 'notaryVerificationRequest',
+           organizationName: 'Fleischerei Wurst',
+           organizationStreetAddress: 'Bockwurst Platz 11',
+           organizationZipCode: '01234',
+           organizationCity: 'Wursten',
+           organizationEvanId: 'LKJHGFD',
+           organizationHRB: 'DE213456789'
+        }]
+      }
+    };
+
+    await runtime.mailbox.sendMail(bmail, runtime.activeAccount, '0x74479766e4997F397942cc607dc59f7cE5AC70b2');
 
     console.log('request identification')
   });
