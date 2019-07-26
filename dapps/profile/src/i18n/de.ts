66d064c1edc1670e1aa7fdda80b05d437a69212a
Karl Adler <906623+karladler@users.noreply.github.com>
2019-07-26 08:29:36 +0200

    CORE-324 translated errors
    - thanks to g-translate :)

--- dapps/profile/src/i18n/de.ts
+++ dapps/profile/src/i18n/de.ts
@@ -179,12 +179,12 @@ export const de = {
       "pay": "Kostenpflichtig bestellen",
       "payment-error": {
         "unknown_state": "Es ist ein Fehler beim Bezahlvorgang aufgetreten. Bitte kontaktieren Sie unseren Support.",
-        "transaction_failed": "Payment/charge was successful but transaction on blockchain failed. <b>Your card was debited!</b> Please contact our customer support.",
-        "charge_failed": "A payment cannot be completed because of a failed payment/charge attempt. <b>Your card was NOT debited!</b> Please try again later.",
-        "invalid_customer": "Customer data is invalid. <b>Your card was NOT debited!</b> Please try again later.",
-        "price_not_okay": "Ether price could not be retrieved in back-end. <b>Your card was NOT debited!</b> Please try again later.",
-        "too_many_accounts": "Submitted email already has too many accounts registered for it. <b>Your card was NOT debited!</b> Please contact our customer support.",
-        "wallet_not_enough_funds": "Wallet does not have enough funds to execute payment. <b>Your card was NOT debited!</b> Please try again later."
+        "transaction_failed": "Zahlung / Abbuchung war erfolgreich, aber die Transaktion in der Blockchain ist fehlgeschlagen. <b> Ihre Karte wurde belastet! </ b> Bitte wenden Sie sich an unseren Kundensupport.",
+        "charge_failed": "Eine Zahlung kann aufgrund eines fehlgeschlagenen Zahlungs- / Abbuchungsversuchs nicht ausgeführt werden. <b> Ihre Karte wurde NICHT belastet! </ b> Bitte versuchen Sie es später erneut.",
+        "invalid_customer": "Kundendaten sind ungültig. <b> Ihre Karte wurde NICHT belastet! </ b> Bitte versuchen Sie es später erneut.",
+        "price_not_okay": "Der Ether-Preis konnte im Back-End nicht abgerufen werden. <b> Ihre Karte wurde NICHT belastet! </ b> Bitte versuchen Sie es später erneut.",
+        "too_many_accounts": "Für die übermittelte E-Mail sind bereits zu viele Konten registriert. <b> Ihre Karte wurde NICHT belastet! </ b> Bitte wenden Sie sich an unseren Kundensupport.",
+        "wallet_not_enough_funds": "Das Wallet verfügt nicht über genügend Guthaben, um die Zahlung auszuführen. <b> Ihre Karte wurde NICHT belastet! </ b> Bitte versuchen Sie es später erneut"
       },
       "successful-payment": "Ihre Bestellung wurde erfolgreich ausgelöst. <br> Sie erhalten eine Rechnung an die angegebene Email Adresse. <br> Der Vorgang ist abgeschlossen, sobald Sie die Rechnung erhalten. <br />Die Gutschrift in Ihrer Wallet erfolgt in den nächsten Minuten.",
       "sepa-acceptance": "Indem Sie Ihre IBAN angeben und diese Zahlung bestätigen, ermächtigen Sie Evan GmbH und Stripe, unseren Zahlungsdienstleister, Anweisungen an Ihre Bank zu senden, um Ihr Konto zu belasten, und Ihre Bank, um Ihr Konto gemäß diesen Anweisungen zu belasten. Sie haben Anspruch auf eine Rückerstattung von Ihrer Bank gemäß den Bedingungen Ihrer Vereinbarung mit Ihrer Bank. Eine Rückerstattung muss innerhalb von 8 Wochen ab dem Datum, an dem Ihr Konto belastet wurde, beantragt werden."
