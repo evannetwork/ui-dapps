6c85788c90f99ddc8743f38793905b396d9e1899
dtrinh <duc.trinh@evan.team>
2019-10-30 16:07:11 +0100

    Added some cleanup

--- step-definitions/standard-ui/checkbox.js
+++ step-definitions/standard-ui/checkbox.js
@@ -5,6 +5,7 @@ import { When } from 'cucumber';
  * Click on custom checkbox
  */
 When('I click on a custom checkbox at position {string}', async position => {
+  // Click on label element (sibling) of checkbox. Direct clicks don't work.
   await client.elements(
     'css selector',
     "[type='checkbox']:first-of-type + label",
--- step-definitions/standard-ui/select.js
+++ step-definitions/standard-ui/select.js
@@ -9,6 +9,7 @@ When('I click on the Vue Select with label {string}',
     let elementId;
     const xPathSelector = `//*/text()[normalize-space(.) = '${label}']/parent::*`;
 
+    // Get id of select box from "for" attribute of the label
     await client.getAttribute('xpath', xPathSelector, "for", attr => {
         elementId = attr.value;
     });
