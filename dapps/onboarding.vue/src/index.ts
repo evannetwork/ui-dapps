d4886464427f030dc34dfe08d28275b8b6f7dc00
Karl Adler <906623+karladler@users.noreply.github.com>
2019-11-06 17:07:59 +0100

    rename translation files, fix default import of json

--- dapps/onboarding.vue/src/index.ts
+++ dapps/onboarding.vue/src/index.ts
@@ -24,7 +24,7 @@ import { initializeVue, getDomainName } from '@evan.network/ui-vue-core';
 import components from './components/registry';
 import Main from './components/root/root.vue';
 import routes from './routes';
-import translations from './i18n/tranlations';
+import translations from './i18n/translations';
 
 /**
  * StartDapp function that is called by the ui-dapp-browser, including an container and the current
