da4c829047db0aeaff2842618e8d1abf2eef43bb
dtrinh <duc.trinh@evan.team>
2019-11-13 16:14:34 +0100

    Added barebone html

--- dapps/profile.vue/src/components/wallet/wallet.vue
+++ dapps/profile.vue/src/components/wallet/wallet.vue
@@ -1,33 +1,45 @@
-/*
-  Copyright (C) 2018-present evan GmbH.
-
-  This program is free software: you can redistribute it and/or modify it
-  under the terms of the GNU Affero General Public License, version 3,
-  as published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Affero General Public License for more details.
-
-  You should have received a copy of the GNU Affero General Public License
-  along with this program. If not, see http://www.gnu.org/licenses/ or
-  write to the Free Software Foundation, Inc., 51 Franklin Street,
-  Fifth Floor, Boston, MA, 02110-1301 USA, or download the license from
-  the following URL: https://evan.network/license/
-*/
+/* Copyright (C) 2018-present evan GmbH. This program is free software: you can
+redistribute it and/or modify it under the terms of the GNU Affero General
+Public License, version 3, as published by the Free Software Foundation. This
+program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License along
+with this program. If not, see http://www.gnu.org/licenses/ or write to the Free
+Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA,
+02110-1301 USA, or download the license from the following URL:
+https://evan.network/license/ */
 
 <template>
-  <div>
-    <p>Test</p>
+  <div class="p-xxl-11 p-xl-6 p-3">
+    <div class="row">
+      <div class="col-xl-4">
+        <evan-wallet-card :address="$route.params.address" />
+      </div>
+      <div class="col-xl-4 d-flex">
+        <evan-button>Test</evan-button>
+        <evan-button>Test</evan-button>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-xl-11">
+        <h1>Last Transactions</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, dolore
+          distinctio porro eveniet facere eum saepe consequuntur aliquam error,
+          ea, consequatur commodi pariatur. Dolore architecto, quos ipsum quidem
+          id corrupti.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-    import Component from "./wallet";
-    export default Component;
+  import Component from "./wallet";
+  export default Component;
 </script>
 
 <style lang="scss">
-    @import "./wallet.scss";
+  @import "./wallet.scss";
 </style>
