eb4bef8b08aa1988341dbd71db0f8c0ef7cdbdcc
Duc Anh Trinh <duc.trinh@evan.team>
2019-11-13 09:42:03 +0100

    Update step-definitions/standard-scenarios/mnemonic.js
    Co-Authored-By: Karl Adler <906623+karladler@users.noreply.github.com>

--- step-definitions/standard-scenarios/mnemonic.js
+++ step-definitions/standard-scenarios/mnemonic.js
@@ -3,6 +3,7 @@ import { When } from 'cucumber';
 
 When('I enter the mnemonic {string}', async mnemonic => {
   let words = mnemonic.split(' ');
+
   for (let i = 0; i < words.length; i++) {
     await client.setValue(`#mnemonicInput${i}`, words[i]);
   }
