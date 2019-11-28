1760c2d85e55e2986432e5ebf25449a01e36fd22
Daniel Maaß <daniel.maass@evan.team>
2019-11-28 09:02:39 +0100

    add styled error and success notifications

--- dapps/profile.vue/src/components/wallet/buy/BuyEve.vue
+++ dapps/profile.vue/src/components/wallet/buy/BuyEve.vue
@@ -11,132 +11,130 @@ Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA,
 https://evan.network/license/ */
 
 <template>
-  <div>
-    <evan-swipe-panel
-      :hideCloseButton="windowWidth >= 1200"
-      :isOpen="$store.state.uiState.swipePanel === 'buyEve'"
-      :mountId="windowWidth < 1200 ? null : 'dapp-wrapper-sidebar-right'"
-      :showBackdrop="windowWidth < 1200"
-      :title="'_profile.wallet.buy-eve.title' | translate"
-      alignment="right"
-      class="light"
-    >
-      
-      <evan-loading v-if="loading || buying" />
-      <template v-else-if="stripe.success">
-        <evan-success />
-        <p>{{_profile.wallet.buy-eve.success}}</p>
-      </template>
-      <template v-else-if="!!stripe.payError">
-        <evan-failed />
-        <p>{{ `_profile.wallet.buy-eve.${ stripe.payError }` | translate }}</p>
-      </template>
-      
-      <!-- start content -->
-      <template v-else>
-        <evan-modal
-          ref="acceptModal"
-          :maxWidth="'600px'">
-          <template v-slot:header>
-            <h5 class="modal-title">
-              {{ `_profile.wallet.buy-eve.accept.title` | translate }}
-            </h5>
-          </template>
-          <template v-slot:body>
-            <span v-html="
-              $t(`_profile.wallet.buy-eve.accept.description`, {
-                amount: payForm.amount.value.toFixed(2),
-              })
-            " />
-          </template>
-          <template v-slot:footer>
-            <evan-button
-              @click="buyEve(); $refs.acceptModal.hide()"
-              type="primary">
-              {{ '_profile.wallet.buy-eve.accept.send' | translate }}
-            </evan-button>
-          </template>
-        </evan-modal>
+  <evan-swipe-panel
+    class="light"
+    alignment="right"
+    :title="'_profile.wallet.buy-eve.title' | translate"
+    :showBackdrop="windowWidth < 1200"
+    :hideCloseButton="windowWidth >= 1200"
+    :mountId="windowWidth < 1200 ? null : 'dapp-wrapper-sidebar-right'"
+    :isOpen="$store.state.uiState.swipePanel === 'buyEve'"
+  >
+    
+    <evan-loading v-if="loading || buying" />
+    <div class="flex-center text-primary" v-else-if="stripe.success">
+      <div><evan-success /></div>
+      <div>{{_profile.wallet.buy-eve.success | translate}}</div>
+    </div>
+    <div class="flex-center text-red" v-else-if="!!stripe.payError">
+      <div><evan-failed /></div>
+      <div>{{ `_profile.wallet.buy-eve.${ stripe.payError }` | translate }}</div>
+    </div>
+    
+    <!-- start content -->
+    <template v-else>
+      <evan-modal
+        ref="acceptModal"
+        :maxWidth="'600px'">
+        <template v-slot:header>
+          <h5 class="modal-title">
+            {{ `_profile.wallet.buy-eve.accept.title` | translate }}
+          </h5>
+        </template>
+        <template v-slot:body>
+          <span v-html="
+            $t(`_profile.wallet.buy-eve.accept.description`, {
+              amount: payForm.amount.value.toFixed(2),
+            })
+          " />
+        </template>
+        <template v-slot:footer>
+          <evan-button
+            @click="buyEve(); $refs.acceptModal.hide()"
+            type="primary">
+            {{ '_profile.wallet.buy-eve.accept.send' | translate }}
+          </evan-button>
+        </template>
+      </evan-modal>
 
-        <div v-show="step === 0">
-          <evan-form
-            :form="payForm"
-            :i18nScope="'_profile.wallet.buy-eve.payForm'"
-            :onlyForm="true"
-            :stacked="true">
-          </evan-form>
+      <div v-show="step === 0">
+        <evan-form
+          :form="payForm"
+          :i18nScope="'_profile.wallet.buy-eve.payForm'"
+          :onlyForm="true"
+          :stacked="true">
+        </evan-form>
 
-          <div class="my-3">
-            <div class="stripeElementLabel">
-              {{ `_profile.wallet.buy-eve.payForm.type.${ payForm.type.value }` | translate }}
-            </div>
-            <div id="stripeElement" class="stripeElement"/>
-            <small
-              class="text-muted"
-              v-if="stripe.error && stripe.error.code">
-              {{ `_profile.wallet.buy-eve.stripe-element.${ stripe.error.code }` | translate }}
-            </small>
-            <small v-show="payForm.type.value === 'iban'">
-              {{ '_profile.wallet.buy-eve.disclaimer' | translate }}
-            </small>
+        <div class="my-3">
+          <div class="stripeElementLabel">
+            {{ `_profile.wallet.buy-eve.payForm.type.${ payForm.type.value }` | translate }}
           </div>
+          <div id="stripeElement" class="stripeElement"/>
+          <small
+            class="text-muted"
+            v-if="stripe.error && stripe.error.code">
+            {{ `_profile.wallet.buy-eve.stripe-element.${ stripe.error.code }` | translate }}
+          </small>
+          <small v-show="payForm.type.value === 'iban'">
+            {{ '_profile.wallet.buy-eve.disclaimer' | translate }}
+          </small>
+        </div>
+      </div>
+
+      <div v-show="step === 1">
+        <evan-form
+          :form="contactForm"
+          :i18nScope="'_profile.company.contact'"
+          :onlyForm="true"
+          :stacked="true">
+        </evan-form>
+      </div>
+    </template>
+    <!-- end contnet -->
+
+    <template slot="footer" v-if="!loading">  
+      <evan-button
+        v-if="step === 0"
+        :disabled="!payForm.isValid || !(stripe.element && stripe.complete && !stripe.error)"
+        :label="'_profile.wallet.buy-eve.continue' | translate"
+        @click="step = 1"
+        type="primary"
+      />
+
+      <div class="w-100" v-else>
+        <div class="text-center mb-3">
+          <span class="text-muted mb-3 d-block"
+            v-if="reverseCharge"
+            v-html="$t('_profile.wallet.buy-eve.reverse-charge')"
+          />
+          <small>
+            {{ payForm.amount.value.toFixed(2) }} EVE x
+            {{ '1€' }} + {{ taxValue }}
+            ({{ (parseFloat(payForm.amount.value) / 100 * taxValue).toFixed(2) + '€' }})
+          </small>
+          <h3 class="mt-1">
+            {{ '_profile.wallet.buy-eve.total-amount' | translate }}:
+            {{ (payForm.amount.value + (parseFloat(payForm.amount.value) / 100 * taxValue)).toFixed(2) }} {{ '€' }}
+          </h3>
         </div>
 
-        <div v-show="step === 1">
-          <evan-form
-            :form="contactForm"
-            :i18nScope="'_profile.company.contact'"
-            :onlyForm="true"
-            :stacked="true">
-          </evan-form>
+        <div class="d-flex">
+          <evan-button
+            type="secondary"
+            @click="step--"
+            :label="'_profile.wallet.buy-eve.back' | translate"
+          />
+          <evan-button
+            :disabled="!payForm.isValid || !contactForm.isValid || vatCalcTimeout"
+            :label="'_profile.wallet.buy-eve.buy' | translate"
+            @click="$refs.acceptModal.show()"
+            class="ml-3 btn-block"
+            type="primary"
+          />
         </div>
-      </template>
-      <!-- end contnet -->
-
-      <template slot="footer" v-if="!loading">  
-        <evan-button
-          v-if="step === 0"
-          :disabled="!payForm.isValid || !(stripe.element && stripe.complete && !stripe.error)"
-          :label="'_profile.wallet.buy-eve.continue' | translate"
-          @click="step = 1"
-          type="primary"
-        />
-
-        <div class="w-100" v-else>
-          <div class="text-center mb-3">
-            <span class="text-muted mb-3 d-block"
-              v-if="reverseCharge"
-              v-html="$t('_profile.wallet.buy-eve.reverse-charge')"
-            />
-            <small>
-              {{ payForm.amount.value.toFixed(2) }} EVE x
-              {{ '1€' }} + {{ taxValue }}
-              ({{ (parseFloat(payForm.amount.value) / 100 * taxValue).toFixed(2) + '€' }})
-            </small>
-            <h3 class="mt-1">
-              {{ '_profile.wallet.buy-eve.total-amount' | translate }}:
-              {{ (payForm.amount.value + (parseFloat(payForm.amount.value) / 100 * taxValue)).toFixed(2) }} {{ '€' }}
-            </h3>
-          </div>
-
-          <div class="d-flex">
-            <evan-button
-              type="secondary"
-              @click="step--"
-              :label="'_profile.wallet.buy-eve.back' | translate"
-            />
-            <evan-button
-              :disabled="!payForm.isValid || !contactForm.isValid || vatCalcTimeout"
-              :label="'_profile.wallet.buy-eve.buy' | translate"
-              @click="$refs.acceptModal.show()"
-              class="ml-3 btn-block"
-              type="primary"
-            />
-          </div>
-        </div>
-      </template>
-    </evan-swipe-panel>
-  </div>
+      </div>
+    </template>
+  </evan-swipe-panel>
 </template>
 
 <script lang="ts">
