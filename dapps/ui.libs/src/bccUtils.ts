8891b3695b6cdc244878bf8e381fe9b213c211c6
Daniel Maaß <daniel.maass@evan.team>
2019-12-05 17:10:22 +0100

    add load contacts from addressbook into bccUtils
    -[CORE-770]

--- dapps/ui.libs/src/bccUtils.ts
+++ dapps/ui.libs/src/bccUtils.ts
@@ -55,3 +55,21 @@ export async function getUserAlias(profile: bcc.Profile, accountDetails?: any, r
     return contact ? contact.alias : profileOwner;
   }
 }
+
+export async function getContacts(runtime: any, unfiltered = false) {
+  // load the contacts for the current user, so we can display correct contact alias
+  const addressBook = (await runtime.profile.getAddressBook()).profile;
+
+  const contacts = Object.keys(addressBook).map(key => {
+    return {
+      'label': addressBook[key].alias,
+      'value': key
+    };
+  });
+
+  if (unfiltered) {
+    return contacts;
+  }
+
+  return contacts.filter(entry => entry.value !== runtime.activeAccount && !/\@/.test(entry.value));
+}
