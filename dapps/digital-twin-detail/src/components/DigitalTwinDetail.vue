b0c6df51eaa96e12aaec6d4f5676e29d8dee9ec2
dtrinh <duc.trinh@evan.team>
2020-01-14 17:45:33 +0100

    add dummy dropdown buttons - [CORE-824]

--- dapps/digital-twin-detail/src/components/DigitalTwinDetail.vue
+++ dapps/digital-twin-detail/src/components/DigitalTwinDetail.vue
@@ -35,9 +35,9 @@
                       icon="mdi mdi-dots-vertical"
                     />
                   </template>
-                  <b-dropdown-item href="#">Action</b-dropdown-item>
-                  <b-dropdown-item href="#">Another action</b-dropdown-item>
-                  <b-dropdown-item href="#">Something else here...</b-dropdown-item>
+                  <b-dropdown-item href="#">TODO Clone</b-dropdown-item>
+                  <b-dropdown-item href="#">TODO Export</b-dropdown-item>
+                  <b-dropdown-item href="#">TODO Remove</b-dropdown-item>
                 </b-dropdown>
               </div>
 
