5c13e617919106f0116cdbe9fad6038267d9aadc
Tobias Winkler <tobias.winkler@evan.team>
2020-02-04 15:37:40 +0100

    remove file-type from ui.libs; format eslint profile-picture
    - [CORE-950]

--- core/ui.libs/src/Files.ts
+++ core/ui.libs/src/Files.ts
@@ -18,7 +18,6 @@
 */
 
 import * as bcc from '@evan.network/api-blockchain-core';
-import FileType from 'file-type/browser';
 
 export interface UIContainerFile extends bcc.ContainerFile {
   /**
@@ -42,50 +41,6 @@ export interface UIContainerFile extends bcc.ContainerFile {
   blob: Blob;
 }
 
-/**
- * Takes an usual File object (e.g. File from HTML 5 file input) and transforms it into an
- * blockchain-core understandable file.
- *
- * @param      {File}  file    html 5 input result file
- */
-export async function fileToContainerFile(
-  file: any,
-): Promise<UIContainerFile> {
-  // use html 5 files and already saved files whit another structure
-  const fileOrigin = file.file ? file.file : file;
-  const type = file.type || file.fileType;
-
-  // use the correct url creator, search for the correct buffer object and create a blob and blob
-  // uri
-  const urlCreator = (<any>window).URL || (<any>window).webkitURL;
-  const buffer = fileOrigin.buffer ? fileOrigin.buffer : await readFileAsArrayBuffer(fileOrigin);
-  const blob = new Blob([ buffer ], { type });
-
-  return {
-    blob: blob,
-    blobUri: urlCreator.createObjectURL(blob),
-    file: buffer,
-    fileType: type,
-    name: file.name,
-    readableSize: getReadableFileSize(file.size),
-    size: file.size,
-  };
-}
-
-export async function blobUriFromIpfsHash(runtime: bcc.Runtime, hash: string) {
-  // @ts-ignore
-  const buffer = await runtime.dfs.get(hash, true);
-
-  return blobUriFromBuffer(buffer);
-}
-
-export async function blobUriFromBuffer(buffer: Buffer) {
-  const urlCreator = (<any>window).URL || (<any>window).webkitURL;
-  const fileType = await FileType.fromBuffer(buffer);
-  const blob = new Blob([buffer], fileType as BlobPropertyBag);
-
-  return urlCreator.createObjectURL(blob);
-}
 
 /**
  * Upload a file that were selected with an HTML 5 <input type="file"> selector or using the
@@ -97,19 +52,17 @@ export async function blobUriFromBuffer(buffer: Buffer) {
 export async function readFileAsArrayBuffer(file: File): Promise<any> {
   if (file instanceof ArrayBuffer) {
     return file;
-  } else {
-    return await new Promise(resolve => {
-      const fileReader = new FileReader();
-
-      // when the file was loaded successfully, return the uploaded file
-      fileReader.onloadend = ($event: any) => resolve($event.target.result);
-
-      // start file reading
-      fileReader.readAsArrayBuffer(file);
-    });
   }
-}
+  return new Promise((resolve) => {
+    const fileReader = new FileReader();
 
+    // when the file was loaded successfully, return the uploaded file
+    fileReader.onloadend = ($event: any) => resolve($event.target.result);
+
+    // start file reading
+    fileReader.readAsArrayBuffer(file);
+  });
+}
 
 /**
  * Parse the file size to a human readable format
@@ -117,14 +70,43 @@ export async function readFileAsArrayBuffer(file: File): Promise<any> {
  * @param      {number}  size    size in B
  * @return     {string}  XXX KB / XXX MB
  */
-export function getReadableFileSize(size: number) {
+export function getReadableFileSize(size: number): string {
   if ((size / 1000000) > 1) {
-    return `${ (size / 1000000).toFixed(2) } MB`;
-  } else if ((size / 1000) > 1) {
-    return `${ (size / 1000).toFixed(2) } KB`;
-  } else {
-    return `${ size.toFixed(2) }B`;
+    return `${(size / 1000000).toFixed(2)} MB`;
+  } if ((size / 1000) > 1) {
+    return `${(size / 1000).toFixed(2)} KB`;
   }
+  return `${size.toFixed(2)}B`;
+}
+
+/**
+ * Takes an usual File object (e.g. File from HTML 5 file input) and transforms it into an
+ * blockchain-core understandable file.
+ *
+ * @param      {File}  file    html 5 input result file
+ */
+export async function fileToContainerFile(
+  file: any,
+): Promise<UIContainerFile> {
+  // use html 5 files and already saved files whit another structure
+  const fileOrigin = file.file ? file.file : file;
+  const { type } = file;
+
+  /* use the correct url creator, search for the correct buffer object and create a blob and blob
+     uri */
+  const urlCreator = (window as any).URL || (window as any).webkitURL;
+  const buffer = fileOrigin.buffer ? fileOrigin.buffer : await readFileAsArrayBuffer(fileOrigin);
+  const blob = new Blob([buffer], { type });
+
+  return {
+    blob,
+    blobUri: urlCreator.createObjectURL(blob),
+    file: buffer,
+    fileType: type,
+    name: file.name,
+    readableSize: getReadableFileSize(file.size),
+    size: file.size,
+  };
 }
 
 /**
@@ -135,12 +117,12 @@ export function getReadableFileSize(size: number) {
  *                                   1000, max_height: 1000)
  * @return     {blob}    Returns the resized img as a blob.
  */
-export async function resizeImage(dataUri: string, dimensions = { max_width: 1000, max_height: 1000 }) {
-  return new Promise((resolve, reject) => {
+export async function resizeImage(dataUri: string, dimensions = { maxWidth: 1000, maxHeight: 1000 }): Promise<any> {
+  return new Promise((resolve) => {
     const img = new Image();
     img.src = dataUri;
 
-    img.onload = function() {
+    img.onload = function (): void {
       const canvas = document.createElement('canvas');
       const ctx = canvas.getContext('2d');
       const canvasCopy = document.createElement('canvas');
@@ -162,7 +144,7 @@ export async function resizeImage(dataUri: string, dimensions = { max_width: 100
       ctx.drawImage(
         canvasCopy,
         0, 0, canvasCopy.width, canvasCopy.height,
-        0, 0, canvas.width, canvas.height
+        0, 0, canvas.width, canvas.height,
       );
       canvas.toBlob((blob) => resolve(blob));
     };
