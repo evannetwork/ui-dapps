9aeaa626424edb06b01ff2b5b3335185f3245187
dtrinh <duc.trinh@evan.team>
2020-02-27 16:31:11 +0100

    add mock data - [CORE-1026]

--- dapps/profile.vue/src/components/did/DID.ts
+++ dapps/profile.vue/src/components/did/DID.ts
@@ -19,7 +19,31 @@
 
 import Component, { mixins } from 'vue-class-component';
 import { EvanComponent } from '@evan.network/ui-vue-core';
-import { Did, DidOptions, Runtime } from '@evan.network/api-blockchain-core';
+import { Runtime, DidDocumentTemplate } from '@evan.network/api-blockchain-core';
+
+const TEST_DID_DOC: DidDocumentTemplate = {
+  '@context': 'https://w3id.org/did/v1',
+  id: 'did:evan:testcore:0x126E901F6F408f5E260d95c62E7c73D9B60fd734',
+  publicKey: [
+    {
+      id: 'did:evan:testcore:0x126E901F6F408f5E260d95c62E7c73D9B60fd734#key-1',
+      type: 'Secp256k1SignatureVerificationKey2018',
+      publicKeyHex: '045adfd502c0bc55f4fcb90eea36368d7e19c5b3045aa6f51dfa3699046e9751251d21bc6bdd06c1ff0014fcbbf9f1d83c714434f2b33d713aaf46760f2d53f10d',
+    },
+  ],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  authentication: [
+    'did:evan:testcore:0x126E901F6F408f5E260d95c62E7c73D9B60fd734#key-1',
+  ],
+  service: [
+    {
+      id: 'did:evan:testcore:0x126E901F6F408f5E260d95c62E7c73D9B60fd734#randomService',
+      type: 'randomService-770853367',
+      serviceEndpoint: 'https://openid.example.com/770853367',
+    },
+  ],
+};
 
 @Component
 export default class DIDComponent extends mixins(EvanComponent) {
@@ -27,24 +51,29 @@ export default class DIDComponent extends mixins(EvanComponent) {
 
   runtime: Runtime = null;
 
+  // TODO: remove mock
+  testDidDoc = TEST_DID_DOC;
+
   /**
    * TODO: handle profiles without creatded did?
    */
-  async fetchDidDocument() {
-    this.runtime = this.getRuntime();
+  async fetchDidDocument(): Promise<DidDocumentTemplate> {
+    // this.runtime = this.getRuntime();
 
-    console.log(this.runtime.nameResolver.config);
+    // console.log(this.runtime.nameResolver.config);
 
-    const didInstance = new Did(this.runtime as DidOptions);
+    // const didInstance = new Did(this.runtime as DidOptions);
 
-    console.log('didInstance', didInstance);
-    const did = await didInstance.convertIdentityToDid(this.runtime.activeIdentity);
+    /* console.log('didInstance', didInstance);
+       const did = await didInstance.convertIdentityToDid(this.runtime.activeIdentity); */
 
-    console.log('did', did);
-    this.didDocument = await didInstance.getDidDocument(did);
-    // this.didDocument = await this.runtime.did.getDidDocument(did);
+    /* console.log('did', did);
+       this.didDocument = await didInstance.getDidDocument(did);
+       // this.didDocument = await this.runtime.did.getDidDocument(did); */
 
-    console.log('didDocument', this.didDocument);
+    // console.log('didDocument', this.didDocument);
+
+    return this.testDidDoc;
   }
 
   async created(): Promise<void> {
