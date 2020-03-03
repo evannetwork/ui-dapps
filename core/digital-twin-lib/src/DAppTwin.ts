df4e6736af45b617c48d5374bf10d7f11ccd8d58
dtrinh <duc.trinh@evan.team>
2020-03-03 11:13:10 +0100

    add back some promise all for performance - [CORE-1077]

--- core/digital-twin-lib/src/DAppTwin.ts
+++ core/digital-twin-lib/src/DAppTwin.ts
@@ -178,10 +178,12 @@ class DAppTwin extends DigitalTwin {
    */
   public async initialize(): Promise<void> {
     await this.loadBaseInfo();
-    this.favorite = await this.isFavorite();
     this.ownerName = await profileUtils.getUserAlias(this.runtime, this.ownerAddress);
-    this.ensureContainers();
-    this.ensureDispatcherStates();
+    this.favorite = await this.isFavorite();
+    await Promise.all([
+      this.ensureContainers(),
+      this.ensureDispatcherStates(),
+    ]);
   }
 
   /**
