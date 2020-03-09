49a3e3e88600d8cf8bec1e5894a6ee3b433f7217
Tobias Winkler <tobias.winkler@evan.team>
2020-03-09 09:55:43 +0100

    remove mnemonic export bottom bar
    - [CORE-1122]

--- core/evancore.vue.libs/src/components/dapp-wrapper/mnemonic-export/mnemonic-export.vue
+++ core/evancore.vue.libs/src/components/dapp-wrapper/mnemonic-export/mnemonic-export.vue
@@ -18,20 +18,7 @@
 */
 
 <template>
-  <div
-    v-if="mnemonic"
-    class="notification-bar"
-  >
-    <div
-      role="button"
-      class="hint-banner bg-primary bg-text-primary col-md-8 offset-md-2 p-2 text-center"
-      @click="showModal"
-    >
-      <i class="mdi mdi-shield-alert-outline" />
-      <span>{{ '_evan.mnemonic-export.notification' | translate }}</span>
-      <i class="mdi mdi-arrow-expand" />
-    </div>
-
+  <div v-if="mnemonic">
     <evan-modal
       ref="modal"
       :close-action="onModalClose"
